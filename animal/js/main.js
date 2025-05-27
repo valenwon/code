b135149bc8c7f6e93fb64f09a13245aaa1fd2d17
valenwon <phw7076@gmail.com>
2025-05-27 13:20:28 +0900

    250527 오전수업 완

--- animal/js/main.js
+++ animal/js/main.js
@@ -1,5 +1,64 @@
 
 $(document).ready(function(){
+    
+    /***************************  header & 메뉴 시작  *******************************
+    - pc인지 모바일인지 구분 - 브라우저 넓이로
+    - 스크롤값 계산
+    - 공통사항 : 브라우저가 스크롤되면 or header에 오버하면 header에 fixed 클래스 추가
+    - pc일 때 : 마우스를 오버한 li에만 over 클래스 추가
+    - 모바일 때 : 메뉴열기를 클릭하면 header에 menu_open 클래스 추가
+                 1차메뉴를 클릭하면 (하위메뉴가 있는 1차메뉴만) 클릭한 li에 open클래스 추가
+    *****/
+    
+    let device_status //모바일인지 pc인지
+    let scrolling //스크롤한 값
+    let window_w //브라우저 넓이
+    let mobile_size = 1024 //모바일로 전환되는 사이즈
+    
+    scroll_chk() //함수 실행 (처음에 문서가 로딩되었을 때 1번)
+    resize_chk() //함수 실행
+    $(window).resize(function(){ //브라우저가 리사이즈 될 떄마다 1번씩 실행
+        resize_chk() //함수 실행
+    })
+    $(window).scroll(function(){ //브라우저를 스크롤 할 때마다 1번씩 실행
+        scroll_chk() //함수 실행
+    })
+    
+    function scroll_chk(){ //함수 선언
+        scrolling = $(window).scrollTop()
+        if(scrolling > 0){
+            $('header').addClass('fixed')
+        }else{
+            $('header').removeClass('fixed')
+        }
+    }
+    function resize_chk(){
+        window_w = $(window).width()
+        if(window_w > mobile_size){
+            device_status = 'pc'
+        }else{
+            device_status = 'mobile'
+        }
+        //console.log(device_status)
+    }
+    
+    /*******header에 마우스를 오버했을 때 *******/
+    $('header').on('mouseenter', function(){
+        console.log('오버햤더')
+        $('header').addClass('fixed')
+    })
+    $('header').on('mouseleave', function(){
+        /*브라우저가 스크롤된 상태에서는 header에 fixed클래스를 삭제하면 XX 
+          맨 위에 있을 때만 삭제해야 함*/
+        if(scrolling <= 0){ 
+            console.log('아웃')
+            $('header').removeClass('fixed')
+        } //if 종료 
+    })
+    
+    /***************************  header & 메뉴 끝  **************************************/
+    
+    /***************************  visual_swiper 시작  ************************************/
     const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
 
         autoplay: {  /* 팝업 자동 실행 */
@@ -31,5 +90,6 @@ $(document).ready(function(){
         $(this).hide() //재생버튼 자신은 숨김
         $('.visual .btn_wrap button.btn_stop').show() //정지 나타남
     })
+    /****************************  visual_swiper 끝  ************************************/
 })
 
