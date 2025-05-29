a9ea1e63bf4e27c92cbb9ce3dd32addea70de096
valenwon <phw7076@gmail.com>
2025-05-29 16:23:40 +0900

    .

--- animal/js/main.js
+++ animal/js/main.js
@@ -179,8 +179,49 @@ $(document).ready(function(){
     /*************  분양 swiper 끝!  ********************/
 
 
-    
+    /**********************************review swiper 시작  ********************************************/
+    const review_swiper = new Swiper('.review .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            768: {    
+                slidesPerView: 2,    /*  'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 24,
+            },
+            1024: {  
+                slidesPerView: 3,    /*  'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 24,
+            },
+        },
+        //centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        // autoplay: {  /* 팝업 자동 실행 */
+        //     delay: 2500,
+        //     disableOnInteraction: true,
+        // },
+        navigation: {
+            nextEl: '.review .list .btn_next',
+            prevEl: '.review .list .btn_prev',
+        },
+    });
+    // review_swiper.autoplay.stop();  /* 일시정지 기능 */
+    // review_swiper.autoplay.start();  /* 재생 기능 */
 
+    /*************  review swiper 끝!  ********************/
+
+
+
+    /**********************************footer top 시작  ********************************************/
+    $('footer .top').on('click', function(){
+        //console.log('클릭')
+        //$(window).scrollTop(0)
+        $('html, body').animate({
+            scrollTop: 0
+        }, 500) //0.5초
+    })
+
+
+    /*************  footer topr 끝!  ********************/
 
 })
 
