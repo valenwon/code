67aed4b94c01210d8c4aebefbf7977c6fa7daf0c
valenwon <phw7076@gmail.com>
2025-06-05 15:04:52 +0900

    250605 중간 수업 완

--- kumho/js/main.js
+++ kumho/js/main.js
@@ -34,11 +34,15 @@ $(document).ready(function(){
 
     /*-----------------news swiper :: 시작 -----------------------*/
     const news_swiper = new Swiper('.news .swiper', { /* 팝업을 감싼는 요소의 class명 */
-        slidesPerView: 2, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        slidesPerView: 'auto', 
         spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
         breakpoints: {
-            640: {    /* 640px 이상일때 적용 */
-                slidesPerView: 3,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+            768: {    /* 768px 이상일때 적용 */
+                slidesPerView: 2, 
+                spaceBetween: 24,
+            },
+            1024: {    /* 768px 이상일때 적용 */
+                slidesPerView: 3, 
                 spaceBetween: 24,
             },
         },
@@ -49,16 +53,40 @@ $(document).ready(function(){
         //     disableOnInteraction: true,
         // },
         navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
+            nextEl: '.news .ctrl_wrap .btn_next',
+            prevEl: '.news .ctrl_wrap .btn_prev',
         },
         pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
-            el: '.swiper-pagination', /* 해당 요소의 class명 */
-            clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+            el: '.news .ctrl_wrap .count', /* 해당 요소의 class명 */
             type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
+        },
+        scrollbar: {
+            el: ".news .ctrl_wrap .swiper-scrollbar",
+            hide: false,
+            draggable: true,
+            dragSize: '100', /*스크롤바 넓이*/
         },
     });
 
     /*-----------------news swiper :: 끝 -----------------------*/
+
+
+    /*-----------------service 배경 변경:: 시작 ------------------
+    - .service .list ul li a 에 마우스를 오버해서 a에 있는
+    - data-name값을 가져다가 list의 class명으로 줌
+    -----*/
+
+    let service_name //가져온 data-name 값을 저장
+    $('.service .list ul li a').on('mouseenter', function(){
+        if($(window).width() > 1024){
+            service_name = $(this).attr('data-name')
+            console.log(service_name)
+            $('.service .list').attr('data-bg', service_name)
+        }
+    })
+    $('.service .list').on('mouseleave', function(){
+        $('.service .list').attr('data-bg', '')
+    })
+    /*-----------------service 배경 변경::끝 -----------------------*/
 
 }) //$(document).ready
