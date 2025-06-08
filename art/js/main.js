2dfb00e2cf3e670c6941c8f3de5f90cc56c7e135
valenwon <phw7076@gmail.com>
2025-06-08 17:27:18 +0900

    .

--- art/js/main.js
+++ art/js/main.js
@@ -9,29 +9,22 @@ $(document).ready(function(){
         effect: "fade", /* fade 효과 */
 
         loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
-
+        
         navigation: {  /* 이전, 다음 버튼 */
             nextEl: '.visual .ctrl_wrap button.btn_next',  /* 다음 버튼의 클래스명 */
             prevEl: '.visual .ctrl_wrap button.btn_prev',  
         },
+
     });
     // visual_swiper.autoplay.stop();  /* 일시정지 기능 */
     // visual_swiper.autoplay.start();  /* 재생 기능 */
 
     $('.visual .ctrl_wrap button.btn_stop').on('click', function(){
-        visual_swiper.autoplay.stop();  /* 일시정지 기능 */
-        // console.log('정지버튼 클릭!!')
         $(this).hide()
         $('.visual .ctrl_wrap button.btn_play').show()
     })
     $('.visual .ctrl_wrap button.btn_play').on('click', function(){
-        visual_swiper.autoplay.start();  /* 재생 기능 */
-        //console.log('재생버튼 클릭!!')
         $(this).hide()
         $('.visual .ctrl_wrap button.btn_stop').show()
     })
-
-
-    
-
 }) //$(document).ready
