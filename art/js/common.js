f82494f9f1cd47be2c07beeac0494e2e12a55a04
valenwon <phw7076@gmail.com>
2025-06-08 20:29:37 +0900

    .

--- art/js/common.js
+++ art/js/common.js
@@ -1,38 +1,75 @@
+$(document).ready(function(){
 /* header, footer 공통요소에 들어가는 jabascript / jquery */
-/********************
- * pc버전, 모바일 버전 구분
- * 스크롤 된 값 계산
- * 
- * 스크롤을 내리면 header에 fixed 클래스 추가
- * 메뉴에 마우스를 올리면 menu_over 클래스 추가
- * 메뉴를 오버한 li에 over 클래스 추가
- * 
- * 스크롤을 내릴 때는 gnb_up 클래스 추가
- * 스크롤을 올릴 때는 gnb_up 클래스 삭제
- * ==> 이전에 스크롤값과 현재 스크롤 값을 비교해서
-       현재 값이 더 크면 내려가는 중 (100 -> 200)
-       현재 값이 작으면 올라가는 중 (200 -> 100)
- * ************** */
+/***************************  header & 메뉴 시작  *******************************
+    - pc인지 모바일인지 구분 - 브라우저 넓이로
+    - 스크롤값 계산
+    - 공통사항 : 브라우저가 스크롤되면 or header에 오버하면 header에 fixed 클래스 추가
+    - pc일 때 : 마우스를 오버한 li에만 over 클래스 추가
+    - 모바일 때 : 메뉴열기를 클릭하면 header에 menu_open 클래스 추가
+                 1차메뉴를 클릭하면 (하위메뉴가 있는 1차메뉴만) 클릭한 li에 open클래스 추가
+    *****/
 
-let device_status //pc인지 모바일인지 구분하는 값
-let scrolling //브라우저가 스크롤 된 값
-let scroll_prev //이전에 스크롤 된 값
-let window_w //브라우저의 넓이 값
-let mobile_size = 1024 //모바일로 변경되는 사이트
-let menu_open //모바일에서 사용할 메뉴가 열렸는지 여부
+    let device_status //pc인지 모바일인지 구분하는 값
+    let scrolling //브라우저가 스크롤 된 값
+    let scroll_prev //이전에 스크롤 된 값
+    let window_w //브라우저의 넓이 값
+    let mobile_size = 1024 //모바일로 변경되는 사이트
 
-$(window).scroll(function(){ //브라우저가 스크롤 될 때마다 1번 실행
-    //console.log('브라우저가 스크롤 된다!!')
-    scroll_chk()
-})
-$(window).resize(function(){ ///리사이즈 될 때마다 1번 실행
-    //console.log('브라우저 크기 변한다!!')
-    resize_chk() //함수의 실행
-})
-$(document).ready(function(){ //문서가 로딩 되고 단 1번
-    //console.log('로딩됐다!!')
-    resize_chk() //함수의 실행
-    scroll_chk()
+    scroll_chk() //함수 실행 (처음에 문서가 로딩되었을 때 1번)
+    resize_chk() //함수 실행
+    $(window).resize(function(){ //브라우저가 리사이즈 될 떄마다 1번씩 실행
+        resize_chk() //함수 실행
+    })
+    $(window).scroll(function(){ //브라우저를 스크롤 할 때마다 1번씩 실행
+        scroll_chk() //함수 실행
+    })
 
-    
+    function scroll_chk (){
+        scroll_prev = scrolling
+        scrolling = $(window).scrollTop()
+        if(scrolling > 0){
+            $('header').addClass('fixed')
+            if(scrolling > scroll_prev){
+                $('header').addClass('gnb_up')
+            }else{
+                $('header').removeClass('gnb_up')
+            }
+        }else{
+            $('header').removeClass('fixed')
+        }
+    }
+
+    function resize_chk (){
+        window_w = $(window).width()
+        if(window_w > mobile_size){
+            device_status = 'pc'
+        }else{
+            device_status = 'mobile'
+        }
+    }
+
+    /*******header에 마우스를 오버했을 때 - 클릭했을 때도 작동함*******/
+    $('header').on('mouseenter focusin', function(){
+        if(device_status == 'pc'){
+            $('header').addClass('fixed')
+        }
+    })
+    $('header').on('mouseleave', function(){
+        if(scrolling <= 0){
+            $('header').removeClass('fixed')
+        }
+    })
+
+    $('header .gnb .gnb_wrap ul.depth1 > li').on('mouseenter focusin', function(){
+        if(device_status == 'pc'){
+            $(this).addClass('over')
+        }
+    })
+    $('header .gnb .gnb_wrap ul.depth1 > li').on('mouseleave', function(){
+        $(this).removeClass('over')
+    })
+    $('header .gnb .gnb_wrap ul.depth1 > li > ul.depth2 > li:last-child').on('focusout', function(){
+        $('header .gnb .gnb_wrap ul.depth1 > li').removeClass('over')
+    })
+
 })
