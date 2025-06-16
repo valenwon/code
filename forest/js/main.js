791a3a8bb59f3854de0c010fded7e3cf5d54c2b0
valenwon <phw7076@gmail.com>
2025-06-16 18:00:56 +0900

    250616 수업 완

---
+++ forest/js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    const myFullpage = new fullpage('#fullpage', {  /* html에서 페이지 전체를 감싸는 요소 */
+
+		navigation: true, /* 오른쪽에 각 페이지의 paging */
+		navigationPosition: 'left', /* 위치 */
+		navigationTooltips: ['Main', '나무심기', '숲 활동', '활동이야기'], /* 툴팁 */
+		showActiveTooltip: true, /* 현재 활성화된 페이지의 툴팁에 특정 클래스 주기 */
+		
+		lockAnchors: true,
+		anchors: ['link1', 'link2', 'link3', 'link4'], /* href="#link1" 이렇게 코딩하면 해당 링크명으로 이동 */
+
+		autoScrolling:true, /* 한페이지씩 스크롤 */
+		scrollHorizontally: true,
+
+		verticalCentered: true, /* 컨텐츠 요소 위아래 가운데 */
+		
+		scrollOverflow: false, /* 컨텐츠가 넘쳐도 스크롤 금지 */
+
+		afterLoad: function(origin, destination, direction, trigger){
+			if(destination.index == 2){ /* index가 2면 슬라이드는 세번째 슬라이드입니다. index 수는 0/1/2/3 */
+				console.log('3번째 슬라이드가 로딩 되었을때');
+			}
+		},
+
+		responsiveWidth: 768, /* fullpage를 적용시키지 않을 모바일 사이즈 */
+        responsiveHeight: 700 /*브라우저 높이가 700이하로 줄면 fullpage안함 */
+	});
+})
