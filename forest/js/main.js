e6818be03bbffbf0c61d4943adb534e2f83e7fb4
valenwon <phw7076@gmail.com>
2025-06-18 15:47:16 +0900

    .

--- forest/js/main.js
+++ forest/js/main.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 		navigationTooltips: ['Main', '나무심기', '숲 활동', '활동이야기', 'footer'], /* 툴팁 */
 		showActiveTooltip: true, /* 현재 활성화된 페이지의 툴팁에 특정 클래스 주기 */
 		
-		lockAnchors: true,
+		lockAnchors: false,
 		anchors: ['main', 'tree', 'work', 'story', 'footer'], /* href="#link1" 이렇게 코딩하면 해당 링크명으로 이동 */
 
 		autoScrolling:true, /* 한페이지씩 스크롤 */
@@ -18,13 +18,25 @@ $(document).ready(function(){
 		scrollOverflow: false, /* 컨텐츠가 넘쳐도 스크롤 금지 */
 
 		afterLoad: function(origin, destination, direction, trigger){
-			if(destination.index == 2){ /* index가 2면 슬라이드는 세번째 슬라이드입니다. index 수는 0/1/2/3 */
-				console.log('3번째 슬라이드가 로딩 되었을때');
+			if(destination.index == 0){ /* index가 2면 슬라이드는 세번째 슬라이드입니다. index 수는 0/1/2/3 */
+				//console.log('1번째 슬라이드가 로딩 되었을때');
+				$('body').removeClass('bg_white')
+			}else if(destination.index == 1){ /* index가 2면 슬라이드는 세번째 슬라이드입니다. index 수는 0/1/2/3 */
+				//console.log('2번째 슬라이드가 로딩 되었을때');
+				$('body').addClass('bg_white')
+				/********************   tree 숫자가 넘어가는 애니메이션******************** */ 
+				$('.counter').counterUp();
+			}else if(destination.index == 2){ /* index가 2면 슬라이드는 세번째 슬라이드입니다. index 수는 0/1/2/3 */
+				//console.log('3번째 슬라이드가 로딩 되었을때');
+				$('body').removeClass('bg_white')
+			} else if(destination.index == 3){ /* index가 2면 슬라이드는 세번째 슬라이드입니다. index 수는 0/1/2/3 */
+				//console.log('4번째 슬라이드가 로딩 되었을때');
+				$('body').addClass('bg_white')
 			}
 		},
 
-		responsiveWidth: 768, /* fullpage를 적용시키지 않을 모바일 사이즈 */
-        responsiveHeight: 700 /*브라우저 높이가 700이하로 줄면 fullpage안함 */
+		responsiveWidth: 1025, /* fullpage를 적용시키지 않을 모바일 사이즈 (1024부터 모바일) */
+        responsiveHeight: 700 /* 브라우저 높이가 700이하로 줄면 fullpage안함 */
 	}); //fullpage
 
 
@@ -49,8 +61,8 @@ $(document).ready(function(){
 	})
 
 
-	/********************   visual swioer 추가******************** */ 
-	let visual_name = ['같이가치 매달기부', '서울마이트리', '고목나무이야기', '산림복원 기금 모집']
+	/********************   visual swiper 추가******************** */ 
+	let visual_name = ['같이가치 매달기부', '서울마이트리', '고목나무 이야기', '산림복원 기금모집']
 	//console.log(visual_name[0]) 숫자가 0부터 시작
 	const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
 
@@ -71,8 +83,8 @@ $(document).ready(function(){
 				return '<span class="' + className + '"> ' + visual_name[index] + "</span>";
 			},
 		},
-		
-
 	});
+
+	
 
 })
