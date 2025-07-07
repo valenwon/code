f0246d38e2351d04b6d958185c875611cbb2ff62
valenwon <phw7076@gmail.com>
2025-07-07 18:02:11 +0900

    .

--- chungbuk/js/main.js
+++ chungbuk/js/main.js
@@ -41,6 +41,38 @@ $(document).ready(function(){
     
     //swiper.autoplay.stop();  /* 일시정지 기능 */
     //swiper.autoplay.start();  /* 재생 기능 */
+
+    const visit_swiper = new Swiper('.visit .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            // 640: {    /* 640px 이상일때 적용 */
+            //     slidesPerView: 2,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+            //     spaceBetween: 16,
+            // },
+            768: {    /* 768px 이상일때 적용 */
+                slidesPerView: 3,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 24,
+                // centeredSlides: true, //왼쪽 정렬
+            },
+            1024: {    /* 1024px 이상일때 적용 */
+                slidesPerView: 'auto',    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 24,
+            },
+        },
+        //centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        autoplay: {  /* 팝업 자동 실행 */
+            delay: 2500,
+            disableOnInteraction: true,
+        },
+        navigation: {
+            nextEl: '.visit .ctrl_wrap .btn_next',
+            prevEl: '.visit .ctrl_wrap .btn_prev',
+        },
+    });
+    // swiper.autoplay.stop();  /* 일시정지 기능 */
+    // swiper.autoplay.start();  /* 재생 기능 */
 })
 
 
