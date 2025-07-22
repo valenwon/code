539907f04a50acf33e73702afd434f6436c39ef0
valenwon <phw7076@gmail.com>
2025-07-22 23:14:19 +0900

    .

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
 		showActiveTooltip: true, /* 현재 활성화된 페이지의 툴팁에 특정 클래스 주기 */
 		
 		lockAnchors: true,
-		anchors: ['link1', 'link2', 'link3', 'link4'], /* href="#link1" 이렇게 코딩하면 해당 링크명으로 이동 */
+		anchors: ['visual', 'about', 'index', 'doosan', 'chungbuk', 'samil', 'thank'], /* href="#link1" 이렇게 코딩하면 해당 링크명으로 이동 */
 
 		autoScrolling:true, /* 한페이지씩 스크롤 */
 		scrollHorizontally: true,
@@ -18,8 +18,20 @@ $(document).ready(function(){
 		scrollOverflow: false, /* 컨텐츠가 넘쳐도 스크롤 금지 */
 
 		afterLoad: function(origin, destination, direction, trigger){
-			if(destination.index == 2){ /* index가 2면 슬라이드는 세번째 슬라이드입니다. index 수는 0/1/2/3 */
-				console.log('3번째 슬라이드가 로딩 되었을때');
+			if(destination.index == 0){ /* index가 2면 슬라이드는 세번째 슬라이드입니다. index 수는 0/1/2/3 */
+				// console.log('1번째 슬라이드가 로딩 되었을때');
+				$('body').removeClass('bg_black')
+			}else if(destination.index == 1){
+				$('body').addClass('bg_black')
+			}else if(destination.index == 2){ /* index가 2면 슬라이드는 세번째 슬라이드입니다. index 수는 0/1/2/3 */
+				//console.log('3번째 슬라이드가 로딩 되었을때');
+				$('body').addClass('bg_black')
+			}else if(destination.index == 3){ /* index가 2면 슬라이드는 세번째 슬라이드입니다. index 수는 0/1/2/3 */
+				//console.log('4번째 슬라이드가 로딩 되었을때');
+				$('body').removeClass('bg_black')
+			}else if(destination.index == 4){ /* index가 2면 슬라이드는 세번째 슬라이드입니다. index 수는 0/1/2/3 */
+				//console.log('5번째 슬라이드가 로딩 되었을때');
+				$('body').removeClass('bg_black')
 			}
 		},
 
