150ef50fc468eea61e5f3be2fe642cde1e5a56bc
valenwon <phw7076@gmail.com>
2025-07-23 13:02:38 +0900

    .

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -28,9 +28,13 @@ $(document).ready(function(){
 				$('body').addClass('bg_black')
 			}else if(destination.index == 3){ /* index가 2면 슬라이드는 세번째 슬라이드입니다. index 수는 0/1/2/3 */
 				//console.log('4번째 슬라이드가 로딩 되었을때');
-				$('body').removeClass('bg_black')
+				$('body').addClass('bg_black')
 			}else if(destination.index == 4){ /* index가 2면 슬라이드는 세번째 슬라이드입니다. index 수는 0/1/2/3 */
 				//console.log('5번째 슬라이드가 로딩 되었을때');
+				$('body').addClass('bg_black')
+			}
+			else if(destination.index == 5){ /* index가 2면 슬라이드는 세번째 슬라이드입니다. index 수는 0/1/2/3 */
+				//console.log('6번째 슬라이드가 로딩 되었을때');
 				$('body').removeClass('bg_black')
 			}
 		},
@@ -40,5 +44,20 @@ $(document).ready(function(){
 	});
 
 
+	/*마우스 커서 이벤트*/
+	$(window).on('scroll mousemove', function(e){  /* html cursor가 마우스 포인터를 따라다니게 하는 값 */
+		$('.cursor').css('left', e.pageX + 'px');
+		$('.cursor').css('top', e.pageY + 'px');
+	});
+
+	$('.photo1').hover(function(){ /* 특정한 요소에 마우스를 올렸을때만 on 클래스 주기 */
+		$('.cursor').toggleClass('photo1');
+	});
+	$('.photo2').hover(function(){ /* 특정한 요소에 마우스를 올렸을때만 on 클래스 주기 */
+		$('.cursor').toggleClass('photo2');
+	});
+	$('.photo3').hover(function(){ /* 특정한 요소에 마우스를 올렸을때만 on 클래스 주기 */
+		$('.cursor').toggleClass('photo3');
+	});
 
 })
